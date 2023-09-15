9d155178b09705af364f23fd3f07e5fac845540b
seanh <seanhughes1230@gmail.com>
2023-09-15 09:54:08 -0500

    removing random statement

--- app/src/main/java/com/cs407/helloworld2/MainActivity.java
+++ app/src/main/java/com/cs407/helloworld2/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
